8f5c90104d1686f71e6012b144a847d2bc90e2c8
DarwoUnicorn <Darwin.Watterson.1994@gmail.com>
2022-05-16 21:05:27 +0300

    Доработал ежедневное вознаграждение и добавил окно

--- Assets/Scripts/Ability/DailyReward.cs
+++ Assets/Scripts/Ability/DailyReward.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 using System;
+using TMPro;
 
 public class DailyReward : MonoBehaviour, IPersistent
 {
+    [SerializeField]
+    private UnityEvent IsGetReward = new UnityEvent();
+
     [SerializeField]
     private HammerAbility _hammerAbility;
     [SerializeField]
     private TimerRestoreAbility _timeRestoreAbility;
     [SerializeReference]
     private DescreaseMaxStartValueAbility _decreaseMaxStartValueAbility;
+    [SerializeField]
+    private TMP_Text _hammerText;
+    [SerializeField]
+    private TMP_Text _timeRestoreText;
+    [SerializeField]
+    private TMP_Text _decreaseMaxStartValueText;
+    [SerializeField]
+    private GameObject _hammerObject;
+    [SerializeField]
+    private GameObject _timeRestoreObject;
+    [SerializeField]
+    private GameObject _decreaseMaxStartValueObject;
     [SerializeField]
     private string _id;
     [SerializeField] [HideInInspector]
@@ -30,7 +47,10 @@ public class DailyReward : MonoBehaviour, IPersistent
         }
         else
         {
-            _day++;
+            if(_day < 6)
+            {
+                _day++;
+            }
         }
         _previousRewardTime = DateTime.Now.Date.ToString();
         GetReward();
@@ -57,11 +77,11 @@ public class DailyReward : MonoBehaviour, IPersistent
         {
             count = 2;
         }
-        else if(_day < 5)
+        else if(_day < 6)
         {
             count = 4;
         }
-        else if(_day < 7)
+        else
         {
             count = 6;
         }
@@ -88,16 +108,23 @@ public class DailyReward : MonoBehaviour, IPersistent
         }
         if(hammerCount != 0)
         {
+            _hammerObject.SetActive(true);
+            _hammerText.text = hammerCount.ToString();
             _hammerAbility.IncreaseAbilityCount(hammerCount);
         }
         if(timeRestoreCount != 0)
         {
+            _timeRestoreObject.SetActive(true);
+            _timeRestoreText.text = timeRestoreCount.ToString();
             _timeRestoreAbility.IncreaseAbilityCount(timeRestoreCount);
         }
         if(descreaseMaxStartValueCount != 0)
         {
+            _decreaseMaxStartValueObject.SetActive(true);
+            _decreaseMaxStartValueText.text = descreaseMaxStartValueCount.ToString();
             _decreaseMaxStartValueAbility.IncreaseAbilityCount(descreaseMaxStartValueCount);
         }
+        IsGetReward?.Invoke();
     }
 
     private void Start()
